f2d782769280e345cc18b0c09c56838c8674bfe3
Cameron Roth <cameron.roth@outlook.com>
2022-05-24 14:35:09 +0800

    Heroku business

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,2 +1,26 @@
+from logging.handlers import RotatingFileHandler
 from app.api import guessprocessing, correct_answer
 from app import app
+
+def create_app(config_class = Config):
+
+    if not app.debug and not app.testing:
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/CITS3403-Project.log',
+            maxBytes = 10240, backupCount = 10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+        
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('CITS3403 Project Sean Maaz Pablo Cam')
+
+    return app
--- config.py
+++ config.py
@@ -4,4 +4,11 @@ basedir= os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY', 'best-unit-evah')
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir,'pixel-perf.db'))
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://' or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    )
+
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     
