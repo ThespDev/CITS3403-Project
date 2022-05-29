0f6bd0b7b57041399a2048df5ede82614e6cbe90
Cameron Roth <cameron.roth@outlook.com>
2022-05-29 23:26:40 +0800

    Unit test structure (Should have committed this ages ago, apologies)

---
+++ tests/test.py
@@ -0,0 +1,10 @@
+import unittest, os
+from app import app, db
+
+class UserModelCase(unittest.TestCase):
+    def setup(self):
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        app.config['SQLALCHEMY_DATABASE_URI'] = \
+            'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
